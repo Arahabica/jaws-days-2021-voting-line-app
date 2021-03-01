ff570768fa9cd07548f82044749dcb28c9c7f5d5
mochan_tk <takashi1.kawamoto@gmail.com>
2021-03-01 13:42:37 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import API from '@aws-amplify/api';
+import Amplify from "aws-amplify";
 import logo from './logo.svg';
 import './App.css';
+import awsmobile from "./aws-exports";
+
+// Amplifyの設定を行う
+Amplify.configure(awsmobile)
 
 function App() {
+  
+  useEffect(() => {
+    console.log("start");
+    // オプション
+    const myInit = {
+        headers: {},
+        response: true
+    };
+    
+    // get LiffId
+    API.get("votingApiGateway", "/items", myInit).then(response => {
+        console.log(response.data.liffId);
+        alert(response.data.liffId)
+    }).catch(error => {
+        console.log(error)
+        alert(error)
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
