a81b2f4710a31356dd6fc29708c1e4f818b44389
EC2 Default User <ec2-user@ip-172-31-42-1.ap-northeast-1.compute.internal>
2021-03-01 15:40:42 +0000

    react liff

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import API from '@aws-amplify/api';
+import Amplify from "aws-amplify";
 import logo from './logo.svg';
+import liff from '@line/liff';
 import './App.css';
+import awsmobile from "./aws-exports";
+
+// Amplifyの設定を行う
+Amplify.configure(awsmobile)
 
 function App() {
+  const [userName, setUserName] = useState<string>("");
+  
+  useEffect(() => {
+    console.log("start");
+    // オプション
+    const myInit = {
+        headers: {},
+        response: true
+    };
+    
+    const fn = async () => {
+      // get LiffId
+      await API.get("votingApiGateway", "/items", myInit)
+      .then(response => {
+        liff.init({liffId: response.data.liffId})
+        if (!liff.isLoggedIn()) {
+          liff.login()
+        }
+        // プロフィール取得
+        liff.getProfile()
+        .then(profile => {
+          setUserName(profile.displayName)
+        })
+        .catch((err) => {
+          console.log('error', err)
+        })
+      })
+      .catch(error => {
+          console.log(error)
+          alert(error)
+      });
+      
+    }
+    fn()
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{userName}</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
