5588573c6e7c2396762f296f594f8d4443604713
EC2 Default User <ec2-user@ip-172-31-42-1.ap-northeast-1.compute.internal>
2021-03-02 07:58:54 +0000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <img src={icon} className="App-logo" alt="iocn" />
         <p>{item} {userName}</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
