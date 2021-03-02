2ad788d4b17d151b379a7443b7c77df4202325dc
EC2 Default User <ec2-user@ip-172-31-42-1.ap-northeast-1.compute.internal>
2021-03-02 11:04:39 +0000

    commit

--- amplify/backend/function/votingLambda/src/app.js
+++ amplify/backend/function/votingLambda/src/app.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
@@ -5,244 +6,61 @@ Licensed under the Apache License, Version 2.0 (the "License"). You may not use 
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 */
-
-
-
-const AWS = require('aws-sdk')
-var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
-var bodyParser = require('body-parser')
-var express = require('express')
-
-AWS.config.update({ region: process.env.TABLE_REGION });
-
-const dynamodb = new AWS.DynamoDB.DocumentClient();
-
-let tableName = "User";
-if(process.env.ENV && process.env.ENV !== "NONE") {
-  tableName = tableName + '-' + process.env.ENV;
+exports.__esModule = true;
+var aws_sdk_1 = require("aws-sdk");
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+var body_parser_1 = require("body-parser");
+var express_1 = require("express");
+aws_sdk_1["default"].config.update({ region: process.env.TABLE_REGION });
+var dynamodb = new aws_sdk_1["default"].DynamoDB.DocumentClient();
+var tableName = "User";
+if (process.env.ENV && process.env.ENV !== "NONE") {
+    tableName = tableName + '-' + process.env.ENV;
 }
-
-const userIdPresent = false; // TODO: update in case is required to use that definition
-const partitionKeyName = "user_id";
-const partitionKeyType = "S";
-const sortKeyName = "";
-const sortKeyType = "";
-const hasSortKey = sortKeyName !== "";
-const path = "/liffid";
-const UNAUTH = 'UNAUTH';
-const hashKeyPath = '/:' + partitionKeyName;
-const sortKeyPath = hasSortKey ? '/:' + sortKeyName : '';
+var userIdPresent = false; // TODO: update in case is required to use that definition
+var partitionKeyName = "user_id";
+var partitionKeyType = "S";
+var sortKeyName = "";
+var sortKeyType = "";
+var hasSortKey = sortKeyName !== "";
+var path = "/liffid";
+var UNAUTH = 'UNAUTH';
+var hashKeyPath = '/:' + partitionKeyName;
+var sortKeyPath = hasSortKey ? '/:' + sortKeyName : '';
 // declare a new express app
-var app = express()
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
-
+var app = express_1["default"]();
+app.use(body_parser_1["default"].json());
+app.use(awsServerlessExpressMiddleware.eventContext());
 // Enable CORS for all methods
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next()
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
 });
-
 // convert url string param to expected Type
-const convertUrlType = (param, type) => {
-  switch(type) {
-    case "N":
-      return Number.parseInt(param);
-    default:
-      return param;
-  }
-}
-
-app.get("/liffid", function(req, res) {
-  res.json({liffId : process.env.LIFF_ID});
-  
+var convertUrlType = function (param, type) {
+    switch (type) {
+        case "N":
+            return Number.parseInt(param);
+        default:
+            return param;
+    }
+};
+app.get("/liffid", function (req, res) {
+    res.json({ liffId: process.env.LIFF_ID });
 });
-
-app.get("/hello", function(req, res) {
-  res.json({item : "Hello！"});
+app.get("/hello", function (req, res) {
+    res.json({ item: "Hello！" });
 });
-
 app.post("/hello", function (req, res) {
-  console.log("★idToken : " + req.body['idToken'])
-  console.log("★accessToken : " + req.body['accessToken'])
-  res.json({ item: "Hello！" });
+    console.log("★idToken : " + req.body['idToken']);
+    console.log("★accessToken : " + req.body['accessToken']);
+    res.json({ item: "Hello！" });
 });
-
-/********************************
- * HTTP Get method for list objects *
- ********************************/
-
-app.get(path + hashKeyPath, function(req, res) {
-  var condition = {}
-  condition[partitionKeyName] = {
-    ComparisonOperator: 'EQ'
-  }
-
-  if (userIdPresent && req.apiGateway) {
-    condition[partitionKeyName]['AttributeValueList'] = [req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH ];
-  } else {
-    try {
-      condition[partitionKeyName]['AttributeValueList'] = [ convertUrlType(req.params[partitionKeyName], partitionKeyType) ];
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-
-  let queryParams = {
-    TableName: tableName,
-    KeyConditions: condition
-  }
-
-  dynamodb.query(queryParams, (err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.json({error: 'Could not load items: ' + err});
-    } else {
-      res.json(data.Items);
-    }
-  });
+app.listen(3000, function () {
+    console.log("App started");
 });
-
-/*****************************************
- * HTTP Get method for get single object *
- *****************************************/
-
-app.get(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
-  var params = {};
-  if (userIdPresent && req.apiGateway) {
-    params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  } else {
-    params[partitionKeyName] = req.params[partitionKeyName];
-    try {
-      params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-
-  let getItemParams = {
-    TableName: tableName,
-    Key: params
-  }
-
-  dynamodb.get(getItemParams,(err, data) => {
-    if(err) {
-      res.statusCode = 500;
-      res.json({error: 'Could not load items: ' + err.message});
-    } else {
-      if (data.Item) {
-        res.json(data.Item);
-      } else {
-        res.json(data) ;
-      }
-    }
-  });
-});
-
-
-/************************************
-* HTTP put method for insert object *
-*************************************/
-
-app.put(path, function(req, res) {
-
-  if (userIdPresent) {
-    req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  }
-
-  let putItemParams = {
-    TableName: tableName,
-    Item: req.body
-  }
-  dynamodb.put(putItemParams, (err, data) => {
-    if(err) {
-      res.statusCode = 500;
-      res.json({error: err, url: req.url, body: req.body});
-    } else{
-      res.json({success: 'put call succeed!', url: req.url, data: data})
-    }
-  });
-});
-
-/************************************
-* HTTP post method for insert object *
-*************************************/
-/*
-app.post(path, function(req, res) {
-
-  if (userIdPresent) {
-    req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  }
-
-  let putItemParams = {
-    TableName: tableName,
-    Item: req.body
-  }
-  dynamodb.put(putItemParams, (err, data) => {
-    if(err) {
-      res.statusCode = 500;
-      res.json({error: err, url: req.url, body: req.body});
-    } else{
-      res.json({success: 'post call succeed!', url: req.url, data: data})
-    }
-  });
-});
-*/
-/**************************************
-* HTTP remove method to delete object *
-***************************************/
-
-app.delete(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
-  var params = {};
-  if (userIdPresent && req.apiGateway) {
-    params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  } else {
-    params[partitionKeyName] = req.params[partitionKeyName];
-     try {
-      params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-
-  let removeItemParams = {
-    TableName: tableName,
-    Key: params
-  }
-  dynamodb.delete(removeItemParams, (err, data)=> {
-    if(err) {
-      res.statusCode = 500;
-      res.json({error: err, url: req.url});
-    } else {
-      res.json({url: req.url, data: data});
-    }
-  });
-});
-app.listen(3000, function() {
-    console.log("App started")
-});
-
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
-module.exports = app
+module.exports = app;
--- amplify/backend/function/votingLambda/src/index.js
+++ amplify/backend/function/votingLambda/src/index.js
@@ -1,9 +1,9 @@
-const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./app');
-
-const server = awsServerlessExpress.createServer(app);
-
-exports.handler = (event, context) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
-  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
+"use strict";
+exports.__esModule = true;
+var awsServerlessExpress = require("aws-serverless-express");
+var app = require('./app');
+var server = awsServerlessExpress.createServer(app);
+exports.handler = function (event, context) {
+    console.log("EVENT: " + JSON.stringify(event));
+    return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
 };
