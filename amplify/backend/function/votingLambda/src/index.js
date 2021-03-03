bccc1848fe6d1245dd0d7f218fe749f7992bda7d
EC2 Default User <ec2-user@ip-172-31-46-231.ap-northeast-1.compute.internal>
2021-03-03 02:27:01 +0000

    commit

--- amplify/backend/function/votingLambda/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var awsServerlessExpress = require("aws-serverless-express");
-var app = require('./app');
-var server = awsServerlessExpress.createServer(app);
-exports.handler = function (event, context) {
-    console.log("EVENT: " + JSON.stringify(event));
-    return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
-};
